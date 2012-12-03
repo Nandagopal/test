68a8cf09efd3d7177a1cc076b6a61937ff6bbd94
Nanda Gopal <nandagopal.yemineni@gmail.com>
2012-12-03 17:35:06 +0530

    "Next commit"

--- RProject/src/one/two/three/Test2.java
+++ RProject/src/one/two/three/Test2.java
@@ -1,6 +1,6 @@
 package one.two.three;
 
-public class Test1 {
+public class Test2 {
 
 	/**
 	 * @param args
@@ -8,7 +8,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("first one");
+		System.out.println("next one");
 
 	}
 
