c364ad5d1fc5d5f9b60b77987953e9d29364e9a0
Nandagopal <nandagopal.yemineni@gmail.com>
2012-12-27 19:25:42 +0530

    Update src/one/two/three/Test2.java
    commiting from github

--- src/one/two/three/Test2.java
+++ src/one/two/three/Test2.java
@@ -1,6 +1,5 @@
 package one.two.three;
 
-<<<<<<< HEAD
 public class Test2 {
 
 	/**
@@ -13,16 +12,5 @@ public class Test2 {
 		System.out.println("next two");
 
 	}
-=======
-public class Test2{
-
-
-public static void main(String[] args){
-
-System.out.pringln("Hello from Test2");
-System.out.pringln("Hello from Test2");
-
-}
->>>>>>> 307a3ba90b0acb6e7c248d15719bc13e4b789884
 
 }
